2759325fc24906d3262c9568a6c20756aba9a922
Bogning Rodney <bogningrodney2@gmail.com>
2024-10-07 04:48:26 +0100

    credential provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 const githubId = process.env.GITHUB_ID;
 const githubSecret = process.env.GITHUB_SECRET;
@@ -16,6 +17,38 @@ if (!githubId || !githubSecret || !googleId || !googleSecret) {
 
 export const authConfig = {
   providers: [
+    Credentials({
+            
+      credentials: {
+          email: { label: "Email", type: "email", placeholder: "Email"},
+          password: { label: "Password", type: "password", placeholder: "Password"}
+      },
+
+      async authorize(credentials) {
+
+          let user = null
+          
+          // validate credentials
+          
+          // get user
+
+          user = {
+              id: "",
+              name: "Neyrod",
+              email: "neyrod@gmail.com"
+          }
+
+          if(!user){
+
+              console.log("Invalis credentials")
+
+              return null
+          }
+
+
+          return user
+      }
+  }),
     GitHubProvider({
       clientId: githubId,
       clientSecret: githubSecret,
@@ -24,6 +57,7 @@ export const authConfig = {
       clientId: googleId,
       clientSecret: googleSecret,
     }),
+
   ],
   adapter: PrismaAdapter(prisma),
 } satisfies NextAuthOptions;
